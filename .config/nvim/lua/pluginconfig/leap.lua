3ba2712ea98934c1028edd9869d98af4da15094b
Luca Benci <benci.luca@tuta.io>
2024-04-26 22:47:32 +0200

    Less context-dependent leap
    Just use "s" to jump backward and forwards

--- .config/nvim/lua/pluginconfig/leap.lua
+++ .config/nvim/lua/pluginconfig/leap.lua
@@ -2,7 +2,11 @@ local M = {}
 
 M.init = function()
 	local leap = require("leap")
-	leap.add_default_mappings()
+
+	vim.keymap.set("n", "s", "<Plug>(leap)")
+	vim.keymap.set("n", "gs", "<Plug>(leap-from-window)")
+	vim.keymap.set({"x", "o"}, "s", "<Plug>(leap-forward)")
+	vim.keymap.set({"x", "o"}, "S", "<Plug>(leap-backward)")
 end
 
 return M
