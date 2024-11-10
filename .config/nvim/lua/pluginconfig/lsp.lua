a61168e91ec5a7735da9029d9663baf61fc7dbe8
Luca Benci <benci.luca@tuta.io>
2024-11-10 17:41:25 +0100

    Update zls.json and enable lsp actions for zig

--- .config/nvim/lua/pluginconfig/lsp.lua
+++ .config/nvim/lua/pluginconfig/lsp.lua
@@ -1,7 +1,6 @@
 local M = {}
 
-local clangd_opts = {
-	on_attach = function(client, bufnr)
+local attach_common_opts = function(client, bufnr)
 		vim.opt.cmdheight = 2
 		vim.opt.updatetime = 300
 		vim.opt.shortmess = vim.opt.shortmess + "c"
@@ -9,7 +8,6 @@ local clangd_opts = {
 
 		-- code navigation
 		vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<CMD>Telescope lsp_definitions<CR>", { noremap = true, silent = true })
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "go", "<CMD>ClangdSwitchSourceHeader<CR>", { noremap = true, silent = true })
 		vim.api.nvim_buf_set_keymap(bufnr, "n", "gy", "<CMD>Telescope lsp_type_definitions<CR>", { noremap = true, silent = true })
 		vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<CMD>Telescope lsp_references<CR>", { noremap = true, silent = true })
 		vim.api.nvim_buf_set_keymap(bufnr, "n", "g?", "<CMD>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
@@ -18,6 +16,14 @@ local clangd_opts = {
 		-- edit actions
 		vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>r", "<CMD>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
 		vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<CMD>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
+end
+
+local clangd_opts = {
+	on_attach = function(client, bufnr)
+		attach_common_opts(client, bufnr)
+
+		-- code navigation
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "go", "<CMD>ClangdSwitchSourceHeader<CR>", { noremap = true, silent = true })
 	end,
 
 	flags = {
@@ -36,7 +42,9 @@ local clangd_opts = {
 	},
 }
 
-local zls_opts = {}
+local zls_opts = {
+	on_attach = attach_common_opts
+}
 
 M.config = function()
 	local lspconfig = require("lspconfig")
