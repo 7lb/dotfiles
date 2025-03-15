74499d3cfe761ca9abfe031b48a7b3d0b158df8a
Luca Benci <benci.luca@tuta.io>
2025-03-15 23:56:58 +0100

    Port gitsigns

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	opts = {
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text_pos = "right_align",
+			delay = 100,
+		},
+		current_line_blame_formatter = "<abbrev_sha> <author>, <author_time:%Y-%m-%d> - <summary>",
+	},
+}
