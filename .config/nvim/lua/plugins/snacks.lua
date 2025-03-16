7e7da5ebc259aea701de16bc23298ef9a7800f0a
Luca Benci <benci.luca@tuta.io>
2025-03-16 15:20:28 +0100

    Replace notify and dressing with snacks

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		indent = { enabled = true, animate = { enabled = false } },
+		input = { enabled = true },
+		notifier = { enabled = true },
+		picker = { enabled = true },
+		quickfile = { enabled = true },
+	},
+}
