fe00f21398050931a34c77ccc448c3c5c978d850
Luca Benci <benci.luca@tuta.io>
2025-03-16 12:31:54 +0100

    Port leap

--- .config/nvim/lua/mappings-plugins.lua
+++ .config/nvim/lua/mappings-plugins.lua
@@ -1,3 +1,7 @@
 -- fzf-lua
 vim.keymap.set("n", "<C-x>", ":FzfLua builtin<cr>")
 vim.keymap.set("n", "<C-p>", ":FzfLua files<cr>")
+
+-- leap
+vim.keymap.set("n", "s", "<plug>(leap)")
+vim.keymap.set("n", "gs", "<plug>(leap-from-window)")
