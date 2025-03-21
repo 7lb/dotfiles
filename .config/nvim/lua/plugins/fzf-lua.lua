726f7af9cf940e3be9190f5ec0b6ab68accc4f17
Luca Benci <benci.luca@tuta.io>
2025-03-21 13:02:35 +0100

    Add way to change root dir when searching files

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,5 +1,27 @@
 return {
 	"ibhagwan/fzf-lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	opts = {
+		actions = {
+			files = {
+				true, -- inherit default actions
+				["ctrl-p"] = {
+					header = "change root",
+					fn = function()
+						local fzf = require("fzf-lua")
+						local prefix = "/home"
+						fzf.fzf_exec("fd --no-ignore --type d", {
+							prompt = "Dirs❯ ",
+							cwd = prefix,
+							actions = {
+								["default"] = function(selected, opts)
+									fzf.files({ cwd = prefix .. '/' .. selected[1] })
+								end,
+							}
+						})
+					end,
+				},
+			},
+		},
+	},
 }
